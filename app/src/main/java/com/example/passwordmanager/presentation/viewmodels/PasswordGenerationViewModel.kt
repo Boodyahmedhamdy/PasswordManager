8e81ba1affecb1eb36d66851a66ce8eeea1f95cb
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-04-28 16:02:08 +0300

    used hilt with use case

--- app/src/main/java/com/example/passwordmanager/presentation/viewmodels/PasswordGenerationViewModel.kt
+++ app/src/main/java/com/example/passwordmanager/presentation/viewmodels/PasswordGenerationViewModel.kt
@@ -1,18 +1,37 @@
 package com.example.passwordmanager.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.passwordmanager.domain.models.PasswordBuilder
 import com.example.passwordmanager.domain.models.PasswordGenerationConfiguration
+import com.example.passwordmanager.domain.usecases.SavePasswordUseCase
 import com.example.passwordmanager.presentation.states.PasswordGenerationUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
 // make it work with hilt
-class PasswordGenerationViewModel: ViewModel() {
+@HiltViewModel
+class PasswordGenerationViewModel @Inject constructor(
+    private val savePasswordUseCase: SavePasswordUseCase
+): ViewModel() {
 
     private val _state = MutableStateFlow(PasswordGenerationUiState())
     val state = _state.asStateFlow()
+
+
+    fun savePassword(password: String) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                savePasswordUseCase(password)
+            }
+        }
+    }
 
 
     fun generatePassword(configuration: PasswordGenerationConfiguration) {
