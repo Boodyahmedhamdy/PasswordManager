5463735b1928b455c0920b9513f0b5ec1f991409
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-04-28 15:10:29 +0300

    added some attributes to handle appearence of Scaffold elements

--- app/src/main/java/com/example/passwordmanager/presentation/screens/ScreenRoutes.kt
+++ app/src/main/java/com/example/passwordmanager/presentation/screens/ScreenRoutes.kt
@@ -1,10 +1,16 @@
 package com.example.passwordmanager.presentation.screens
 
-sealed class ScreenRoutes(val route: String) {
+sealed class ScreenRoutes(
+    val route: String,
+    val hasTopBar: Boolean = true,
+    val hasBottomBar: Boolean = true,
+    val hasFabButton: Boolean = true
+) {
     data object HomeScreen: ScreenRoutes("homeScreen")
     data object SecurityGateScreen: ScreenRoutes("securityScreen")
     data object PasswordPreviewScreen: ScreenRoutes("passwordPreviewScreen")
     data object PasswordConfigurationScreen: ScreenRoutes("passwordConfigurationScreen")
     data object PasswordDetailsScreen: ScreenRoutes("passwordDetailsScreen")
     data object PasswordEditScreen: ScreenRoutes("passwordEditScreen")
+
 }
