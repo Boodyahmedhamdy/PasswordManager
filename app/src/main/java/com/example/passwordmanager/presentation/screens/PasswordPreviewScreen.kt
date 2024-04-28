58ffcdb18641a86c213839f5a7dcebb5be9525d4
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-04-28 15:12:45 +0300

    preview screen initial body

--- app/src/main/java/com/example/passwordmanager/presentation/screens/PasswordPreviewScreen.kt
+++ app/src/main/java/com/example/passwordmanager/presentation/screens/PasswordPreviewScreen.kt
@@ -1,8 +1,23 @@
 package com.example.passwordmanager.presentation.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.passwordmanager.presentation.states.PasswordPreviewScreenUiState
 
 @Composable
-fun PasswordPreviewScreen() {
-
+fun PasswordPreviewScreen(
+    state: PasswordPreviewScreenUiState,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = state.password, style = MaterialTheme.typography.headlineMedium)
+    }
 }
