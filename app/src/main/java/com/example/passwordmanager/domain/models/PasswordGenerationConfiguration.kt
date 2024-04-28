9baf54bdec0a8af2d8b233707d970cf1d0156109
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-04-28 15:09:27 +0300

    optimized imports

--- app/src/main/java/com/example/passwordmanager/domain/models/PasswordGenerationConfiguration.kt
+++ app/src/main/java/com/example/passwordmanager/domain/models/PasswordGenerationConfiguration.kt
@@ -1,5 +1,7 @@
 package com.example.passwordmanager.domain.models
 
+import javax.inject.Inject
+
 data class PasswordGenerationConfiguration(
     val length: Int = 16,
     val caseSensitive: Boolean = true,
