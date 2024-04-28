7630f87a2edbf0614b5803db08984203952252de
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-04-28 15:14:51 +0300

    updated depending on previous changes and added generatePassword function

--- app/src/main/java/com/example/passwordmanager/presentation/viewmodels/PasswordGenerationViewModel.kt
+++ app/src/main/java/com/example/passwordmanager/presentation/viewmodels/PasswordGenerationViewModel.kt
@@ -1,8 +1,9 @@
 package com.example.passwordmanager.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.passwordmanager.domain.models.PasswordBuilder
 import com.example.passwordmanager.domain.models.PasswordGenerationConfiguration
-import com.example.passwordmanager.presentation.states.PasswordConfigurationScreenUiState
+import com.example.passwordmanager.presentation.states.PasswordGenerationUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -10,47 +11,71 @@ import kotlinx.coroutines.flow.update
 // make it work with hilt
 class PasswordGenerationViewModel: ViewModel() {
 
-    private val _state = MutableStateFlow(PasswordConfigurationScreenUiState())
+    private val _state = MutableStateFlow(PasswordGenerationUiState())
     val state = _state.asStateFlow()
 
+
+    fun generatePassword(configuration: PasswordGenerationConfiguration) {
+        val password = PasswordBuilder.build(configuration).content
+        updateGeneratedPassword(password)
+    }
+
+    fun updateGeneratedPassword(newPassword: String) {
+        _state.update {
+            it.copy(
+                passwordPreviewScreenUiState = it.passwordPreviewScreenUiState.copy(
+                    password = newPassword
+                )
+            )
+        }
+    }
 
     // configuration updates
     fun updateLength(newLength: Int) {
 
         _state.update {
             it.copy(
-                configuration = it.configuration.copy(length = newLength)
+                passwordConfigurationScreenUiState =
+                it.passwordConfigurationScreenUiState.copy(
+                    configuration =
+                    it.passwordConfigurationScreenUiState.configuration.copy(length = newLength)
+                ),
             )
         }
     }
     fun updateHasNumbers(newHasNumbers: Boolean) {
         _state.update {
             it.copy(
-                configuration = it.configuration.copy(
-                    hasNumbers = newHasNumbers
-                )
+                passwordConfigurationScreenUiState =
+                it.passwordConfigurationScreenUiState.copy(
+                    configuration =
+                    it.passwordConfigurationScreenUiState.configuration.copy(hasNumbers = newHasNumbers)
+                ),
             )
         }
     }
     fun updateSpecialCharacters(newSpecialCharacters: Boolean) {
         _state.update {
             it.copy(
-                configuration = it.configuration.copy(
-                    hasSpecialCharacters = newSpecialCharacters
-                )
+                passwordConfigurationScreenUiState =
+                it.passwordConfigurationScreenUiState.copy(
+                    configuration =
+                    it.passwordConfigurationScreenUiState.configuration.copy(
+                        hasSpecialCharacters = newSpecialCharacters
+                    )
+                ),
             )
         }
     }
     fun updateCaseSensitive(newCaseSensitive: Boolean) {
         _state.update {
             it.copy(
-                configuration = it.configuration.copy(
-                    caseSensitive = newCaseSensitive
-                )
+                passwordConfigurationScreenUiState =
+                it.passwordConfigurationScreenUiState.copy(
+                    configuration =
+                    it.passwordConfigurationScreenUiState.configuration.copy(caseSensitive = newCaseSensitive)
+                ),
             )
         }
     }
-
-
-
 }
