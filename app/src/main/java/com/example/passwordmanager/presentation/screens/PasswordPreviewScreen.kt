c4e6b87934886e235ee6369d8bb1385fb3629c40
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-04-28 16:00:44 +0300

    created the screen body

--- app/src/main/java/com/example/passwordmanager/presentation/screens/PasswordPreviewScreen.kt
+++ app/src/main/java/com/example/passwordmanager/presentation/screens/PasswordPreviewScreen.kt
@@ -1,23 +1,69 @@
 package com.example.passwordmanager.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.passwordmanager.presentation.states.PasswordPreviewScreenUiState
 
 @Composable
 fun PasswordPreviewScreen(
     state: PasswordPreviewScreenUiState,
     modifier: Modifier = Modifier,
+    onClickSavePassword: (password: String) -> Unit = {},
+    onClickReGenerate: () -> Unit = {}
 ) {
-    Box(
+
+    Column(
         modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
     ) {
+
+        // password
+        Text(text = "the Generated Password is", style = MaterialTheme.typography.labelLarge)
+
         Text(text = state.password, style = MaterialTheme.typography.headlineMedium)
+
+
+        // row contains 2 buttons
+            // save
+            // regenerate
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            // save button
+            Button(onClick = {
+                val password = state.password
+                onClickSavePassword(password)
+            }) {
+                Text(text = "Save")
+            }
+
+            OutlinedButton(onClick = {
+                onClickReGenerate()
+            }) {
+                Text(text = "ReGenerate")
+            }
+        }
     }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun PasswordPreviewScreenPreview() {
+    PasswordPreviewScreen(state = PasswordPreviewScreenUiState("thisdflksjdflksdfj"))
 }
