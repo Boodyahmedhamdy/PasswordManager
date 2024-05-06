c56bf69d4a29f4f5aaf0601f9bbbb70fc9a95bad
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-05-06 13:18:06 +0300

    fixed createdAt field wasn't set

--- app/src/main/java/com/example/passwordmanager/domain/usecases/SavePasswordUseCase.kt
+++ app/src/main/java/com/example/passwordmanager/domain/usecases/SavePasswordUseCase.kt
@@ -3,6 +3,7 @@ package com.example.passwordmanager.domain.usecases
 import com.example.passwordmanager.data.PasswordDAO
 import com.example.passwordmanager.domain.models.Password
 import com.example.passwordmanager.mappers.toEntity
+import java.util.Date
 import javax.inject.Inject
 
 class SavePasswordUseCase @Inject constructor(
@@ -10,8 +11,11 @@ class SavePasswordUseCase @Inject constructor(
 ) {
 
     operator fun invoke(password: Password) {
+        val date = Date(System.currentTimeMillis())
+        var passwordEntityToBeSaved = password.toEntity()
+        passwordEntityToBeSaved = passwordEntityToBeSaved.copy(createdAt = date)
         passwordDAO.addPassword(
-            password.toEntity()
+            passwordEntityToBeSaved
         )
     }
 
