f8004af60789c731ce07716b3803e356b86f9c9c
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-05-06 14:56:34 +0300

    added title to be at top bar

--- app/src/main/java/com/example/passwordmanager/presentation/screens/ScreenRoutes.kt
+++ app/src/main/java/com/example/passwordmanager/presentation/screens/ScreenRoutes.kt
@@ -4,13 +4,29 @@ sealed class ScreenRoutes(
     val route: String,
     val hasTopBar: Boolean = true,
     val hasBottomBar: Boolean = true,
-    val hasFabButton: Boolean = true
+    val hasFabButton: Boolean = true,
+    val title: String = "Screen"
 ) {
-    data object HomeScreen: ScreenRoutes("homeScreen")
+    data object HomeScreen: ScreenRoutes(
+        "homeScreen",
+        title = "Home"
+    )
     data object SecurityGateScreen: ScreenRoutes("securityScreen")
-    data object PasswordPreviewScreen: ScreenRoutes("passwordPreviewScreen")
-    data object PasswordConfigurationScreen: ScreenRoutes("passwordConfigurationScreen")
-    data object PasswordDetailsScreen: ScreenRoutes("passwordDetailsScreen")
+    data object PasswordPreviewScreen: ScreenRoutes(
+        "passwordPreviewScreen",
+        hasTopBar = false,
+        hasBottomBar = false,
+        hasFabButton = false
+    )
+    data object PasswordConfigurationScreen: ScreenRoutes(
+        "passwordConfigurationScreen",
+        hasTopBar = false,
+        hasBottomBar = false,
+        hasFabButton = false
+    )
+    data object PasswordDetailsScreen: ScreenRoutes(
+        "passwordDetailsScreen"
+    )
     data object PasswordEditScreen: ScreenRoutes("passwordEditScreen")
 
 }
