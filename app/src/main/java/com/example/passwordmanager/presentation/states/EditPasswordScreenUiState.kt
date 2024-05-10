78e1af9df2dcc49fc07c254e6d648f0c88d1c7e2
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-05-10 14:54:56 +0300

    states for Editing password

---
+++ app/src/main/java/com/example/passwordmanager/presentation/states/EditPasswordScreenUiState.kt
@@ -0,0 +1,5 @@
+package com.example.passwordmanager.presentation.states
+
+data class EditPasswordScreenUiState(
+    val password: PasswordUiState = PasswordUiState()
+)
---
+++ app/src/main/java/com/example/passwordmanager/presentation/states/EditPasswordUiState.kt
@@ -0,0 +1,5 @@
+package com.example.passwordmanager.presentation.states
+
+data class EditPasswordUiState(
+    val editPasswordScreenUiState: EditPasswordScreenUiState = EditPasswordScreenUiState()
+)
