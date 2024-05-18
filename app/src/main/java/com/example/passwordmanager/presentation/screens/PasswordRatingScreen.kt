82451f3f454262b7cf2a20c7c9aae6ff0453c49f
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-05-18 13:39:41 +0300

    new placeholder place holder Screen

---
+++ app/src/main/java/com/example/passwordmanager/presentation/screens/PasswordRatingScreen.kt
@@ -0,0 +1,15 @@
+package com.example.passwordmanager.presentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.passwordmanager.presentation.components.PlaceholderScreen
+
+@Composable
+fun PasswordRatingScreen(
+    modifier: Modifier = Modifier
+) {
+    PlaceholderScreen(
+        title = "Password Rating Screen .. coming soon",
+        modifier = modifier
+    )
+}
