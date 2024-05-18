521b44b2e266d9baaec5aa1a0ffc115faa66f4f2
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-05-18 12:47:17 +0300

    placeholder for Settings

---
+++ app/src/main/java/com/example/passwordmanager/presentation/screens/SettingsScreen.kt
@@ -0,0 +1,15 @@
+package com.example.passwordmanager.presentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.passwordmanager.presentation.components.PlaceholderScreen
+
+@Composable
+fun SettingsScreen(
+    modifier: Modifier = Modifier
+) {
+    PlaceholderScreen(
+        title = "Settings Screen .. coming soon",
+        modifier = modifier
+    )
+}
