eb4e9b68707b9c24b46f26d5069fd3e0cad04476
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-05-18 12:47:25 +0300

    placeholder for Edit Screen

--- app/src/main/java/com/example/passwordmanager/presentation/screens/PasswordEditScreen.kt
+++ app/src/main/java/com/example/passwordmanager/presentation/screens/PasswordEditScreen.kt
@@ -1,8 +1,16 @@
 package com.example.passwordmanager.presentation.screens
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.passwordmanager.presentation.components.PlaceholderScreen
 
 @Composable
-fun PasswordEditScreen() {
+fun PasswordEditScreen(
+    modifier: Modifier = Modifier
+) {
+    PlaceholderScreen(
+        title = "Edit Password Screen",
+        modifier = modifier
+    )
 
 }
